2dab464bd0c2e84993b9fe76c7a10faab97b39ee
ramanda.dabain@optimumpartners.co <ramanda.dabain@optimumpartners.co>
2024-02-08 12:16:33 +0300

    Add project files.

---
+++ src/LibraryManagement.Core/LibraryManagementConsts.cs
@@ -0,0 +1,20 @@
+﻿using LibraryManagement.Debugging;
+
+namespace LibraryManagement
+{
+    public class LibraryManagementConsts
+    {
+        public const string LocalizationSourceName = "LibraryManagement";
+
+        public const string ConnectionStringName = "Default";
+
+        public const bool MultiTenancyEnabled = true;
+
+
+        /// <summary>
+        /// Default pass phrase for SimpleStringCipher decrypt/encrypt operations
+        /// </summary>
+        public static readonly string DefaultPassPhrase =
+            DebugHelper.IsDebug ? "gsKxGZ012HLL3MI5" : "6bea6a9f2c5f435eaed8fda98142ca66";
+    }
+}
