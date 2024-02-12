66bfb306327fa02930d05e32ebadba23f29fdd45
ramanda.dabain@optimumpartners.co <ramanda.dabain@optimumpartners.co>
2024-02-12 21:26:59 +0300

    Relations

--- src/LibraryManagement.EntityFrameworkCore/EntityFrameworkCore/LibraryManagementDbContext.cs
+++ src/LibraryManagement.EntityFrameworkCore/EntityFrameworkCore/LibraryManagementDbContext.cs
@@ -3,16 +3,39 @@ using Abp.Zero.EntityFrameworkCore;
 using LibraryManagement.Authorization.Roles;
 using LibraryManagement.Authorization.Users;
 using LibraryManagement.MultiTenancy;
+using LibraryManagement.Models;
 
 namespace LibraryManagement.EntityFrameworkCore
 {
-    public class LibraryManagementDbContext : AbpZeroDbContext<Tenant, Role, User, LibraryManagementDbContext>
-    {
-        /* Define a DbSet for each entity of the application */
-        
-        public LibraryManagementDbContext(DbContextOptions<LibraryManagementDbContext> options)
-            : base(options)
-        {
-        }
-    }
+	public class LibraryManagementDbContext : AbpZeroDbContext<Tenant, Role, User, LibraryManagementDbContext>
+	{
+		/* Define a DbSet for each entity of the application */
+
+		public LibraryManagementDbContext(DbContextOptions<LibraryManagementDbContext> options)
+			: base(options)
+		{
+		}
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+			modelBuilder.Entity<Task>()
+			 .HasOne<User>()
+			 .WithMany()
+			 .HasForeignKey(t => t.CreatorUserId)
+			 .IsRequired()
+			 .OnDelete(DeleteBehavior.Restrict);
+
+			modelBuilder.Entity<Task>()
+				.HasOne<User>()
+				.WithMany()
+				.HasForeignKey(t => t.AssigneeUserId)
+				.IsRequired()
+				.OnDelete(DeleteBehavior.Restrict);
+
+		}
+
+
+		public DbSet<Task> Tasks { get; set; }
+		//public DbSet<UserProfile> Usersdd { get; set; }
+	}
 }
