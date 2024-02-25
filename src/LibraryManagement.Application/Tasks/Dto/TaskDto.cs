a5cfbaa76eceacdb93c0fd69aab33459a93a7003
ramanda.dabain@optimumpartners.co <ramanda.dabain@optimumpartners.co>
2024-02-25 10:10:44 +0300

    Custom-Attribute

--- src/LibraryManagement.Application/Tasks/Dto/TaskDto.cs
+++ src/LibraryManagement.Application/Tasks/Dto/TaskDto.cs
@@ -2,6 +2,7 @@
 using Abp.Domain.Repositories;
 using Abp.Domain.Uow;
 using Abp.Runtime.Validation;
+using LibraryManagement.Entities.CustomAttributes;
 using LibraryManagement.Models.LookUps;
 using System;
 using System.ComponentModel.DataAnnotations;
@@ -9,10 +10,11 @@ using System.Linq;
 
 namespace LibraryManagement.Tasks.Dto
 {
-	public class TaskDto : ICustomValidate
-	{
+	public class TaskDto 
+		{
 		public long Id { get; set; }
 		[Required]
+		[Unique]
 		public string Name { get; set; }
 		[Required]
 
@@ -32,28 +34,5 @@ namespace LibraryManagement.Tasks.Dto
 		public long AssigneeId { get; set; }
 
 
-		public void AddValidationErrors(CustomValidationContext context)
-		{
-			using (var scope = context.IocResolver.CreateScope())
-			{
-				using (var uow = scope.Resolve<IUnitOfWorkManager>().Begin())
-				{
-					var taskRepository = scope.Resolve<IRepository<Entities.Task, long>>();
-
-					var nameExists = taskRepository.GetAll()
-						.Where(s => s.Name == Name && s.Id != Id)
-						.Any();
-
-					if (nameExists)
-					{
-						var errorMessage = "A task with the same name already exists";
-						var memberNames = new[] { nameof(Name) };
-						context.Results.Add(new ValidationResult(errorMessage, memberNames));
-					}
-
-					uow.Complete();
-				}
-			}
-		}
 	}
 }
---
+++ src/LibraryManagement.Core/Entities/CustomAttributes/UniqueAttribute.cs
@@ -0,0 +1,34 @@
+﻿using Abp.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace LibraryManagement.Entities.CustomAttributes8
+{
+	public class UniqueAttribute : ValidationAttribute
+	{
+		private readonly IRepository<Entities.Task, long> _taskRepository;
+		public UniqueAttribute(IRepository<Entities.Task, long> taskRepository)
+		{
+			_taskRepository = taskRepository;
+		}
+
+		protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+		{
+			if (value == null)
+			{
+				return ValidationResult.Success;
+			}
+			var task = _taskRepository.GetAll().Where(i => i.Name == value).FirstOrDefault();
+
+			if (task != null)
+			{
+				return ValidationResult.Success;
+			}
+			return new ValidationResult("A task with the same name already exists");
+
+		}
+	}
+}
